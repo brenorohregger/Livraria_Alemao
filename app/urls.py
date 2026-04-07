9bce3b898a08842e143d3a4213dec65d874a5b28
brenorohregger <brenoot0405@gmail.com>
2026-04-07 15:02:19 -0300

    feat: feito

--- app/urls.py
+++ app/urls.py
@@ -21,6 +21,11 @@ from core.views import (
     UserViewSet,
 )
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from uploader.router import router as uploader_router
+
 router = DefaultRouter()
 
 router.register(r'autores', AutorViewSet, basename='autores')
@@ -51,4 +56,7 @@ urlpatterns = [
     path('api/registro/', UserRegistrationView.as_view(), name='user_registration'),
     # API
     path('api/', include(router.urls)),
+    path('api/media/', include(uploader_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
